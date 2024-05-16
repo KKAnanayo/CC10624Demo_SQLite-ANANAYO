32c158e2d4149170f3f81c787d6efc3c64503c70
hed-kkananayo <jadenyuki486@gmail.com>
2024-05-16 17:39:57 +0800

    Confirmation of delete

--- app/src/main/java/com/example/cc10624demo_sqlite_ananayo/StudentEdit.java
+++ app/src/main/java/com/example/cc10624demo_sqlite_ananayo/StudentEdit.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -29,12 +30,7 @@ public class StudentEdit extends AppCompatActivity {
 
         btnDelete.setOnClickListener(v -> {
             if (student != null) {
-                DBHandler dbHandler = new DBHandler(this);
-                dbHandler.deleteStudent(student.getId());
-                toastMessage("Student deleted successfully");
-
-                Intent delete = new Intent(StudentEdit.this, StudentList.class);
-                startActivity(delete);
+                showDeleteConfirmationDialog(student);
             } else {
                 toastMessage("No student to delete");
             }
@@ -85,7 +81,21 @@ public class StudentEdit extends AppCompatActivity {
         }
     }
 
+    private void showDeleteConfirmationDialog(Student student) {
+        new AlertDialog.Builder(this)
+                .setTitle("Delete Confirmation")
+                .setMessage("Are you sure you want to delete this student?")
+                .setPositiveButton(android.R.string.yes, (dialog, which) -> {
+                    DBHandler dbHandler = new DBHandler(StudentEdit.this);
+                    dbHandler.deleteStudent(student.getId());
+                    toastMessage("Student deleted successfully");
 
+                    Intent delete = new Intent(StudentEdit.this, StudentList.class);
+                    startActivity(delete);
+                })
+                .setNegativeButton(android.R.string.no, null)
+                .show();
+    }
 
     public void toastMessage(String message){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
